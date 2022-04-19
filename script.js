427d63a3607540d4d6fbf2bea4b011cd5a1ffbc2
Aleksander Afonin <alexanderafonin96@gmail.com>
2022-04-19 14:25:18 +0600

    lesson11

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 'use strict'
 
+const text = document.getElementById('text');
+const btn = document.getElementById('btn');
+const square = document.getElementById('square');
+
+const eBtn = document.getElementById('e_btn');
+
+const inputRange = document.getElementById('range');
+const circle = document.getElementById('circle');
+
+//ex.1
+const changeBackgound = function () {
+    square.style.background = text.value;
+}
+
+btn.addEventListener('click', changeBackgound);
+
+//ex.2
+eBtn.style.display = 'none';
+
+//ex.3
+const changeSize = function () {
+    circle.style.width = inputRange.value + "%";
+    circle.style.height = inputRange.value + "%";
+}
+
+inputRange.addEventListener('input', changeSize);
+
